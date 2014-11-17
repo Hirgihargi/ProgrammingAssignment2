8cc1da44287f10df12d5632f11d93ffcc32d2c10
UKa <f1bb89dc@opayq.com>
2014-11-17 17:09:49 +0100

    Correction of the comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-# To avoid the repeated calculation of the inverse of a matrix and thus safe computational effort,
+# To avoid repeated calculations of the inverse matrix and thus to safe computational effort,
 # the result is cached instead.
 
 # The function makeCacheMatrix creates a list of functions to set and get the matrix and its inverse.
@@ -18,7 +18,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 # The function cacheSolve checks if the inverse of the matrix is already been calculated.
-# If that's the case, it gets the result from the charge,
+# If that's the case, it gets the result from the cache,
 # if not it calculates the inverse using the solve function.
 # The inverse matrix is returned.
 
